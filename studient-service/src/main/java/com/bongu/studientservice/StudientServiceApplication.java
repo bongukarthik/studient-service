b9fbf42ecfe731b93facb010289de4474f3e43fd
bongukarthik <38668380+bongukarthik@users.noreply.github.com>
2021-02-22 23:10:25 +0530

    Added boot files

---
+++ studient-service/src/main/java/com/bongu/studientservice/StudientServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bongu.studientservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudientServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudientServiceApplication.class, args);
+	}
+
+}
---
+++ studient-service/src/test/java/com/bongu/studientservice/StudientServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bongu.studientservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudientServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
